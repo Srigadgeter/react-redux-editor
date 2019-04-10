e71809edb6b7257a3316a82c60e681fcc2c61454
Srigadgeter <srigadgets@gmail.com>
2019-04-11 01:34:24 +0530

    Handling Key Commands with the help of RichUtils component

--- src/MyEditorApp.js
+++ src/MyEditorApp.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 import './MyEditorApp.css';
 
 class MyEditorApp extends Component {
@@ -11,11 +11,25 @@ class MyEditorApp extends Component {
     this.onChange = (editorState) => this.setState({ editorState });
   }
   
+  // RichUtils has information of key commands available to web editors
+  handleKeyCommand = (command, editorState) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if(newState){
+      console.log(command); // we could find what action had done by key command. 
+      // It will log value 'bold' if we press 'CTRL+B' (similar to 'italic' & 'underline') after selecting some text.
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
 
   render() {
     return (
       <div className="MyEditorApp">
-        <Editor editorState={this.state.editorState} onChange={this.onChange} />
+        <Editor 
+          editorState={this.state.editorState} 
+          handleKeyCommand={this.handleKeyCommand}
+          onChange={this.onChange} />
       </div>
     );
   }
